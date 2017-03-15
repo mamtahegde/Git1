f0e50682744a041c70067fcb57550786a68ce1d6
Mamta-PC <Mamta-PC@DESKTOP-5FH3MB0>
2017-03-15 21:24:14 +0530

    Uploading Framework

---
+++ src/example/Test1.java
@@ -0,0 +1,11 @@
+package example;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is test 1");
+
+	}
+
+}
---
+++ src/example/Test3.java
@@ -0,0 +1,10 @@
+package example;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is test 3");
+	}
+
+}
